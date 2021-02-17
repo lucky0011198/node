9980157f54c2d9e8250d1099274e8849ce0eac6b
lucky0011198 <68720142+lucky0011198@users.noreply.github.com>
2021-02-17 20:11:37 +0530

    first commit

--- app.js
+++ app.js
@@ -1,17 +1,24 @@
 require('dotenv').config();
 const express = require('express')
 const app = express()
+const port = process.env.PORT || 2000;
+
+
 const path = require("path");
 const hbs = require("hbs");
 const templets_path = path.join(__dirname,"./Templets/views");
 const partials_path = path.join(__dirname, "./Templets/partials");
-const port = process.env.PORT || 2000;
+
 app.set('view engine', '.hbs');
 app.set("views", templets_path);
 hbs.registerPartials(partials_path);
 
+
+
+
+
 app.get('/', (req, res) => {
-  res.render("index");
+  res.render("login");
 })
 
 app.listen(port, () => {
