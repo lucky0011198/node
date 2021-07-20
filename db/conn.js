6cc5526514edb7fda34af80e2319582fca02cfab
laxmikantvarkal <68720142+lucky0011198@users.noreply.github.com>
2021-07-20 12:13:32 +0530

    Update conn.js

--- db/conn.js
+++ db/conn.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-mongoose.connect("mongodb+srv://laxmikant:Lucky@382001@cluster0.o2ltu.mongodb.net/lucky?retryWrites=true&w=majority",{
+mongoose.connect("mongodb+srv://*****************@cluster0.o2ltu.mongodb.net/********?retryWrites=true&w=majority",{
     useNewUrlParser:true,
     useUnifiedTopology:true,
     useCreateIndex:true
